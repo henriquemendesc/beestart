95ac29f36ee556636ed5a6651e1e3327e7671857
Bob <henriiimendesc@gmail.com>
2017-10-23 10:33:03 -0200

    login screen

--- app/src/main/java/com/dev/bob/beestart/Activities/LoginActivity.java
+++ app/src/main/java/com/dev/bob/beestart/Activities/LoginActivity.java
@@ -2,14 +2,24 @@ package com.dev.bob.beestart.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.dev.bob.beestart.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button btnLoginScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLoginScreen = (Button)findViewById(R.id.btnScreenLogin);
+        btnLoginScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
