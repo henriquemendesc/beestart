0c384968d1de6fc8ebd20c31654274dd30a7ec17
Bob <henriiimendesc@gmail.com>
2017-10-23 11:12:44 -0200

    main xml

--- app/src/main/java/com/dev/bob/beestart/Activities/MainActivity.java
+++ app/src/main/java/com/dev/bob/beestart/Activities/MainActivity.java
@@ -1,14 +1,10 @@
 package com.dev.bob.beestart.Activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.dev.bob.beestart.Activities.CadastroActivity;
-import com.dev.bob.beestart.Activities.LoginActivity;
 import com.dev.bob.beestart.R;
 import com.dev.bob.beestart.Util.StartToolBarTitle;
 
