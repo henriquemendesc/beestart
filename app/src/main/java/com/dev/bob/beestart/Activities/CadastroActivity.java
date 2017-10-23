440925b0d76b1b2a0aac4c6c6f92797cf92e9f8c
Bob <henriiimendesc@gmail.com>
2017-10-23 09:25:51 -0200

    first commit

---
+++ app/src/main/java/com/dev/bob/beestart/Activities/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.dev.bob.beestart.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev.bob.beestart.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
